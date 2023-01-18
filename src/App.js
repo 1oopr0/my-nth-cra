241b3e67439f27078348873d0c07a79b51a61ae2
1oopr0 <1oo.ooooo.pro@gmail.com>
2023-01-18 22:26:51 +0900

    coin(temp)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    return () => console.log("api bye");
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
-
-  useEffect(() => console.log("SEARCH FOR", keyword), [keyword]);
-
-  const [rv, setRv] = useState(false);
-  const onRv = () => setRv((p) => !p);
+  const [money, setMoney] = useState(0);
+  function onChange(event) {
+    setMoney(event.target.value);
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+    setMoney(0);
+  }
+  const [selectedCoin, setSelectedCoin] = useState("");
+  function onCoinSelectChange(event) {
+    let option = event.target.options[event.target.selectedIndex];
+    option.selected = true;
+    setSelectedCoin(JSON.parse(option.value));
+  }
+  console.log(coins);
   return (
     <div>
-      {rv ? <Button text="hello" /> : null}
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onRv}>click me</button>
+      <h1> The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong> Loading...</strong>
+      ) : (
+        <div>
+          <label for="coin-select">Choose a coin: </label>
+          <select
+            onChange={onCoinSelectChange}
+            value={JSON.stringify(selectedCoin)}
+            id="coin-select"
+          >
+            <option value="">--Please choose an option--</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={JSON.stringify(coin)}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <hl />
+          <form onSubmit={onSubmit}>
+            <input
+              placeholer="your money..."
+              type="number"
+              onChange={onChange}
+              value={money}
+            ></input>
+            {"$ equals to "}
+            <span>
+              {selectedCoin !== ""
+                ? `${money / selectedCoin.quotes.USD.price} ${
+                    selectedCoin.symbol
+                  }`
+                : null}
+            </span>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
